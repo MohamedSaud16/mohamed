39632ac9498e9ab441134042eff95267a20da17c
MohamedSaud16 <Aljwharh.84.a@gmail.com>
2025-04-19 18:47:52 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Heart)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Hello!")
+})
+basic.showNumber(4)
+basic.pause(1000)
+basic.showNumber(4)
+basic.pause(100)
